37bab9a10707e118199741a25496e2a868d23ba5
Erica Murdock-Waters <ericahpa@gmail.com>
2019-02-19 21:15:02 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,4 @@
 class User
-  attr_accessor :first_name, :last_name, :knowledge
-
-  def initialize
-    @knowledge = []
-  end
-
-  def learn(new_knowledge)
-    @knowledge << new_knowledge
-  end
+  attr_accessor :first_name, :last_name
 
 end
